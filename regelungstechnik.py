3edebc476a288f8b3d1eb89e913455751623bf94
David Haerer <david@haerer.xyz>
2019-04-12 20:39:27 +0200

    Added step response of DigitalPID and fixed polynomial cancelation

--- regelungstechnik.py
+++ regelungstechnik.py
@@ -158,8 +158,8 @@ class Element(object):
         An element is described by the counter and denominator of its transfer function. It has corresponding scipy lti and TransferFunction object as attributes.
         counter and denominator are list from highest order term to lowest.
         """
-        self.counter = counter
-        self.denominator = denominator
+        self.counter = np.asarray(counter, dtype=np.float64)
+        self.denominator = np.asarray(denominator, dtype=np.float64)
         self.sys = lti(counter, denominator)
         self.tf = TransferFunction(counter, denominator)
 
@@ -320,25 +320,22 @@ class PROD(Element):
 
         # Cancel polynomials
 
-        counters = [e.counter for e in self.elements]
-        denominators = [e.denominator for e in self.elements]
+        cs = [e.counter for e in self.elements]
+        ds = [e.denominator for e in self.elements]
 
         for i in range(len(elements)):
             for j in range(len(elements)):
-                if counters[i] == denominators[j]:
-                    counters[i] = [1]
-                    denominators[j] = [1]
+                if cs[i].size == ds[j].size and (cs[i] == ds[j]).all():
+                    cs[i] = [1]
+                    ds[j] = [1]
                     break
-
-        counters.remove([1])
-        denominators.remove([1])
 
         counter, denominator = [1], [1]
 
-        for c in counters:
+        for c in cs:
             counter = polymul(counter, c)
 
-        for d in denominators:
+        for d in ds:
             denominator = polymul(denominator, d)
 
         super().__init__(counter, denominator)
@@ -597,16 +594,18 @@ class StepResponse(object):
 # Digital Controller
 
 class DigitalPID(object):
-    def __init__(self, V, Tn, Tv, delta_t, u_max=None):
+    def __init__(self, V, Tn, Tv, delta_t, umax=None):
+        self.delta_t = delta_t
+
         self.KP = V * (Tn + Tv) / Tn
         self.KI = V * delta_t / Tn
         self.KD = V * Tv / delta_t
 
-        self.umax = u_max
+        self.umax = umax
         self.e_old = 0
         self.ui = 0
 
-    def __call__(self, target, real):
+    def __call__(self, target, real, verbose=False):
         e = target - real
 
         up = self.KP * e
@@ -615,25 +614,54 @@ class DigitalPID(object):
 
         if self.umax is not None:
             ui = min(ui, self.umax)
-            ui = max(ui, -1 * self.umax)
+            ui = max(ui, -self.umax)
 
         ud = self.KD * (e - self.e_old)
 
         u = up + ui + ud
 
+        if self.umax is not None:
+            u = min(u, self.umax)
+            u = max(u, -self.umax)
+
         self.e_old = e
         self.ui = ui
 
+        if verbose:
+            print(f"e={e:.2f} up={up:.2f} ui={ui:.2f} ud={ud:.2f} u={u:.2f}")
+
         return u
 
-    def __str__(self, lang="DE"):
+    def w(self, t):
+        t = np.asarray(t, dtype=np.float64)
+        s = step(t)
+        n = t.size
+        val = np.zeros(n, dtype=np.float64)
+
+        tick = t[0]
+        val[0] = self(s[0], 0)
+
+        for i in range(1, n):
+            if t[i] - tick >= self.delta_t:
+                tick = t[i]
+                val[i] = self(s[i], 0)
+            else:
+                val[i] = val[i - 1]
+
+        return val
+
+    def code(self, lang="DE"):
         target = "soll" if lang == "DE" else "target"
         real = "ist" if lang == "DE" else "real"
         e_old = "e_alt" if lang == "DE" else "e_old"
 
-        code = f"float KP = {self.KP};\n"
-        code += f"float KI = {self.KI};\n"
-        code += f"float KD = {self.KD};\n"
+        code = f"float KP = {self.KP:.2f};\n"
+        code += f"float KI = {self.KI:.2f};\n"
+        code += f"float KD = {self.KD:.2f};\n"
+
+        if self.umax is not None:
+            code += f"float umax = {self.umax};\n"
+
         code += f"\n"
         code += f"float regler(float {target}, float {real}) {{\n"
         code += f"    float e, u, up, ud;\n"
@@ -653,9 +681,22 @@ class DigitalPID(object):
             code += f"\n"
 
         code += f"    u = up + ui + ud;\n"
+        code += f"\n"
+
+        if self.umax is not None:
+            code += f"    if(u > umax);\n"
+            code += f"        u = umax;\n"
+            code += f"    if(u < -umax);\n"
+            code += f"        u = -umax;\n"
+            code += f"\n"
+
         code += f"    {e_old} = e;\n"
         code += f"\n"
         code += f"    return u;\n"
-        code += f"}"
+        code += f"}}"
 
         return code
+
+    def reset(self):
+        self.e_old = 0
+        self.ui = 0
